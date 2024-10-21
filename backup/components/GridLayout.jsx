28006e68a164c6f3c40004c7c4461d57fc20f7bb
telmenganhuyag <134697379+telmenganhuyag@users.noreply.github.com>
2024-10-22 01:39:19 +0800

    Delete backup/components directory

--- backup/components/GridLayout.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const GridLayout = ({ children }) => {
-  return (
-    <div className="relative grid grid-cols-[1fr_3fr] gap-4">{children}</div>
-  );
-};
-
-export default GridLayout;
--- backup/components/PageContainer.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const PageContainer = ({ children }) => {
-  return <div className="min-h-screen bg-white">{children}</div>;
-};
-
-export default PageContainer;
