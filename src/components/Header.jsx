9b48a75554e01281fb559b4acd3623e49938d4a2
Telmen <telmenganhuyag@gmail.com>
2024-10-22 02:15:44 +0800

    updated src path for profile picture

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,17 +5,18 @@ const Header = ({ userInfo }) => {
   return (
     <div className="hero absolute top-16 z-30 flex h-[153px] w-full items-center gap-60 bg-[#103581] shadow-xl">
       <img
-        src="src/assets/profile-pic.webp"
+        src="/react-demo/profile-pic.webp"
         alt="profile-pic"
         className="ml-40 h-52 w-52 rounded-full border-4 border-[#104181] object-cover"
       />
       <div className="flex flex-col text-white">
         <div className="text-6xl font-bold uppercase">
-          {userInfo.name.split(" ")[0]} <br /> &nbsp;&nbsp;&nbsp;&nbsp;{userInfo.name.split(" ")[1]}
+          {userInfo.name.split(" ")[0]} <br /> &nbsp;&nbsp;&nbsp;&nbsp;
+          {userInfo.name.split(" ")[1]}
         </div>
         <p className="self-center font-light">{userInfo.title}</p>
       </div>
-      <div className="self-end ml-16 text-yellow-500">
+      <div className="ml-16 self-end text-yellow-500">
         <Link to="edit">Edit Profile</Link>
       </div>
     </div>
